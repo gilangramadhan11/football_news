9aec22ef00ee1248309a8c3506c2f157bf80f1d6
Gilang Ramadhan <gilangramadan1519@gmail.com>
2026-09-19 20:27:09 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://football-news-web-five.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
