e726e68490c502c3e65e8048075509ad3e950c0c
Gilang Ramadhan <gilangramadan1519@gmail.com>
2026-09-21 16:31:08 +0700

    fix url

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -265,7 +265,13 @@ class HomeController extends Controller
             ->get();
 
         return response()->json([
-            'featured' => $featured,
+            'featured' => $featured->map(function ($article) {
+                $article->thumbnail = $article->thumbnail
+                    ? 'https://footballnews-production.up.railway.app/storage/' . $article->thumbnail
+                    : 'https://footballnews-production.up.railway.app/images/default-news.jpg';
+
+                return $article;
+            }),
             'breakingNews' => $breakingNews,
             'recentArticles' => $recentArticles,
             'weekFixtures' => $weekFixtures,
